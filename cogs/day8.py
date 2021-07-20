c249aa36239777d8bb4b068b5443d49bd6802fa3
Hans Hazairi <hanshazairi@gmail.com>
2021-07-21 00:59:01 +0800

    chore: add 'iplocation' & 'iplocation_2' commands

--- cogs/day8.py
+++ cogs/day8.py
@@ -6,6 +6,11 @@ class Day8(commands.Cog, name='Day 8'):
     def __init__(self, bot):
         self.bot = bot
 
+    def get_ip(self):
+        url = 'https://api.ipify.org/?format=json'
+        data = requests.get(url).json()
+        return data['ip']
+
     @commands.command(hidden=True)
     async def joke(self, ctx):
         """
@@ -24,9 +29,31 @@ class Day8(commands.Cog, name='Day 8'):
         """
         Returns the bot's IPv4 address
         """
-        url = 'https://api.ipify.org/?format=json'
+        ip = self.get_ip()
+        await ctx.reply(ip)
+
+    @commands.command()
+    async def iplocation(self, ctx):
+        """
+        Returns the bot's IPv4 address geolocation
+        """
+        ip = self.get_ip()
+        url = f'https://ipinfo.io/{ip}/geo'
         data = requests.get(url).json()
-        await ctx.reply(data['ip'])
+        await ctx.reply(f'{data["city"]}, {data["region"]}, {data["country"]}')
+
+    @commands.command()
+    async def iplocation_2(self, ctx):
+        """
+        Reacts with a flag corresponding to the bot's IPv4 address geolocation
+        """
+        ip = self.get_ip()
+        url = f'https://ipinfo.io/{ip}/geo'
+        url2 = f'https://api.ip2country.info/ip?{ip}'
+        data = requests.get(url).json()
+        data2 = requests.get(url2).json()
+        await ctx.reply(f'{data["city"]}, {data["region"]}, {data["country"]}')
+        await ctx.message.add_reaction(data2['countryEmoji'])
 
 
 def setup(bot):
