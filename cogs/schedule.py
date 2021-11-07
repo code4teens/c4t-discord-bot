8fda399feb31ffe6409b3370f200dac02ca6036b
Hans Hazairi <hanshazairi@gmail.com>
2021-11-07 18:32:25 +0800

    fix: schedule related jobs

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -14,7 +14,7 @@ class Schedule(commands.Cog):
         self.bot = bot
         self.trigger_loop.start()
 
-    async def assign_peers(self, guild, day, date):
+    async def assign_peers(self, cohort_data, guild, day, date):
         # get & shuffle students
         role_students = get(guild.roles, name='Students')
         students = role_students.members
@@ -32,7 +32,7 @@ class Schedule(commands.Cog):
             user_data = user_r.json()
             chn_eval_id = next(
                 channel['id'] for channel in user_data['channels']
-                if channel['cohort']['id'] == utils.active_cohort['id']
+                if channel['cohort']['id'] == cohort_data['id']
             )
             chn_eval = get(guild.text_channels, id=chn_eval_id)
 
@@ -41,20 +41,19 @@ class Schedule(commands.Cog):
                 if user in students and user.id != evaluatee.id:
                     await chn_eval.set_permissions(user, overwrite=None)
 
-            # discord.py Botcamp schedule
-            if day != 9:
-                # update database
+            if day != cohort_data['duration']:
+                # update eval table
                 eval_url = f'{API_URL}/evals'
-                post_data = {
+                post_eval_data = {
                     'evaluator_id': evaluator.id,
                     'evaluatee_id': evaluatee.id,
                     'cohort_id': utils.active_cohort['id'],
                     'date': date
                 }
-                eval_r = s.post(eval_url, json=post_data, timeout=5)
+                eval_r = s.post(eval_url, json=post_eval_data, timeout=5)
 
                 if eval_r.status_code != requests.codes.created:
-                    eval_r.raise_for_status()  # TODO: log error
+                    eval_r.raise_for_status()
 
                 # grant new tester permission to view evaluatee channel
                 await chn_eval.set_permissions(evaluator, view_channel=True)
@@ -72,15 +71,16 @@ class Schedule(commands.Cog):
                 await chn_eval.send(message)
 
     async def assign_groups(self, guild):
-        def get_village_role(i):
-            return get(guild.roles, name=f'Village {i}')
+        async def create_village_role(i):
+            role = await guild.create_role(f'Village {i}')
+
+            return role
 
         role_students = get(guild.roles, name='Students')
         students = role_students.members
-        roles_villages = [
-            x for x in map(get_village_role, range(1, 11)) if x is not None
-        ]
-        n = len(roles_villages)
+        random.shuffle(students)
+        n = len(students) // 5  # max no. of students per village
+        roles_villages = [x for x in map(create_village_role, range(1, n))]
         chunks = (students[i:i + n] for i in range(0, len(students), n))
 
         for chunk in chunks:
@@ -97,13 +97,14 @@ class Schedule(commands.Cog):
     @tasks.loop(seconds=3600)
     async def check_schedule(self):
         utils.active_cohort = get_active_cohort()
-        start_date = datetime.strptime(
-            utils.active_cohort['start_date'], '%Y-%m-%d'
-        ).astimezone(tz)
+        cohort_data = utils.active_cohort
+        start_date = datetime.strptime(cohort_data['start_date'], '%Y-%m-%d')\
+            .astimezone(tz)
         _now = now()
 
-        # discord.py Botcamp schedule
-        if _now >= start_date and _now < start_date + timedelta(days=9):
+        if _now >= start_date \
+                and _now < start_date + \
+                timedelta(days=cohort_data['duration']):
             delta = _now - start_date
             day = delta.days + 1
 
@@ -111,57 +112,62 @@ class Schedule(commands.Cog):
             chn_server_log = get(guild.text_channels, name='server-log')
             chn_alerts = get(guild.text_channels, name='alerts')
             chn_townhall = get(guild.voice_channels, name='townhall')
-            chn_chit_chat = get(guild.text_channels, name='chit-chat')
             role_students = get(guild.roles, name='Students')
 
-            # everyday @ 7:00 am
-            if _now.hour == 7:
-                await chn_server_log.send(
-                    f'This is a scheduled test message for Day {day}.'
-                )
-            # everyday @ 8:00 am
-            elif _now.hour == 8:
-                if day == 1:
-                    await chn_chit_chat.send(
-                        f'Good Morning {role_students.mention}, this is a '
-                        'reminder for our Townhall at 9:00 am. See you at '
-                        f'{chn_townhall.mention}!'
+            # discord.py Botcamp
+            if cohort_data['id'] in utils.dpy:
+                # everyday @ 7:00 am
+                if _now.hour == 7:
+                    await chn_server_log.send(
+                        'This is a scheduled test message for '
+                        f'{cohort_data["name"]} Day {day}.'
                     )
-                elif day == 4:
-                    await self.assign_groups(guild)
-                    await chn_alerts.send(
-                        f'Good Morning {role_students.mention}, you are put '
-                        'into groups for your Day-09 project. All the best!\n'
+                # everyday @ 8:00 am
+                elif _now.hour == 8:
+                    if day == 1:
+                        await chn_alerts.send(
+                            f'Good Morning {role_students.mention}, this is a '
+                            'reminder for our Townhall at 9:00 am. See you at '
+                            f'{chn_townhall.mention}!'
+                        )
+                    elif day == 4:
+                        await self.assign_groups(guild)
+                        await chn_alerts.send(
+                            f'Good Morning {role_students.mention}, you are '
+                            'put into groups for your Day-09 project. All the '
+                            'best!'
+                        )
+                    elif day == 9:
+                        await chn_alerts.send(
+                            f'Good Morning {role_students.mention}, all the '
+                            'best on your final day!'
+                        )
+                    else:
+                        await chn_alerts.send(
+                            f'Good Morning {role_students.mention}, all the '
+                            'best for today!\n'
+                        )
+                # everyday @ 10:00 am
+                elif _now.hour == 10:
+                    await self.assign_peers(
+                        guild, cohort_data, day, str(_now.date())
                     )
-                elif day == 9:
-                    await chn_chit_chat.send(
-                        f'Good Morning {role_students.mention}, all the best '
-                        'on your final day!'
-                    )
-                else:
-                    await chn_alerts.send(
-                        f'Good Morning {role_students.mention}, all the best '
-                        'for today!\n'
-                    )
-            # everyday @ 11:00 am
-            elif _now.hour == 10:
-                await self.assign_peers(guild, day, str(_now.date()))
-            # days 1 - 8 @ 4:00 pm
-            elif _now.hour == 16:
-                if day != 9:
-                    await chn_alerts.send(
-                        f'Good Evening {role_students.mention}, this is a '
-                        'reminder to discuss your projects with your assigned '
-                        'peers before 6:00 pm.'
-                    )
-            # days 1, 2, 4 & 9 @ 5:00 pm
-            elif _now.hour == 17:
-                if day == 1 or day == 2 or day == 4 or day == 9:
-                    await chn_chit_chat.send(
-                        f'Hi {role_students.mention}, this is a reminder for '
-                        'our Townhall at 6:00 pm. See you at '
-                        f'{chn_townhall.mention}!'
-                    )
+                # days 1 - 8 @ 4:00 pm
+                elif _now.hour == 16:
+                    if day != 9:
+                        await chn_alerts.send(
+                            f'Good Evening {role_students.mention}, this is a '
+                            'reminder to discuss your projects with your '
+                            'assigned peers before 6:00 pm.'
+                        )
+                # days 1, 2, 4 & 9 @ 5:00 pm
+                elif _now.hour == 17:
+                    if day == 1 or day == 2 or day == 4 or day == 9:
+                        await chn_alerts.send(
+                            f'Hi {role_students.mention}, this is a reminder '
+                            'for our Townhall at 6:00 pm. See you at '
+                            f'{chn_townhall.mention}!'
+                        )
 
     @trigger_loop.before_loop
     async def before_trigger_loop(self):
